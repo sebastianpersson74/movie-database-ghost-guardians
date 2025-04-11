7500bc2cb3eeb3c6711e681abd8907a68ff1b944
Sebastian Persson <sebastian@katshing.com>
2025-04-11 11:17:49 +0200

    Lagt till API för moviegrid på startsidan

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import MovieGrid from "./components/MovieGrid";
+import React from "react";
+
+function App() {
+    return (
+        <div>
+            <MovieGrid />
+        </div>
+    )
+};
+
+export default App;
