67e819548965743a2f48e1e40a13df4ba7cfd7a6
Agnes R <agnes.rytiniemi@gmail.com>
2025-04-13 13:49:47 +0200

    skapat ett första utkast av en trailerkarusell med lite styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import Carousel from "./components/Trailers";
 import MovieGrid from "./components/MovieGrid";
 import React from "react";
 
 function App() {
-    return (
-        <div>
-            <MovieGrid />
-        </div>
-    )
-};
+  return (
+    <div>
+      <h1>Trailerkarusell 🎬</h1>
+      <Carousel />
+
+      <MovieGrid />
+    </div>
+  );
+}
 
 export default App;
