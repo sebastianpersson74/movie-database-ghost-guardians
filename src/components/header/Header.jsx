08f6fefd3209877997cdec6d6298a93def6f6ec3
Yenn <yb.burgman@gmail.com>
2025-04-16 13:52:52 +0200

    gjort headern responsiv

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,12 +4,14 @@ import HandleSearch from "../movieSearch/HandleSearch";
 import Logo from "../../assets/logo.png";
 import Favourites from "../../assets/star.svg";
 import { Link } from "react-router-dom";
+import MobileLogo from "../../assets/Logo-small.svg"
 
 const Header = () => {
   return (
     <div className="header">
       <Link to="/">
-        <img src={Logo} className="logo" alt="Logga" />
+        <img src={Logo} className="logo desktop-logo" alt="Logga" />
+        <img src={MobileLogo} className="logo mobile-logo" alt="Filmrulle" />
       </Link>
       <HandleSearch />
       <Link to="/favorites">
